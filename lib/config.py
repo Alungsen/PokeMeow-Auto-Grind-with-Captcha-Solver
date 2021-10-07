7f3db7995e0058954eac5ad4d6ab079c3f9b0a3e
Mehul343 <71486556+Mehul343@users.noreply.github.com>
2021-10-07 18:25:21 +0530

    Update config.py

--- lib/config.py
+++ lib/config.py
@@ -11,7 +11,14 @@ fishing=False #Set it to True to enable Fishing
 
 #List of cheap legies on which you would like to do prb or ub
 #You can add or remove names from this list
-cheap_legies=["Articuno", "Mew", "Jirachi", "Moltres", "Raikou", "Entei", "Suicune", "Ho-oh", "Regirock", "Regice", "Registeel", "Kyogre", "Latias", "Latios", "Deoxys", "Uxie", "Mesprit", "Azelf", "Heatran", "Regigigas", "Cresselia", "Cobalion", "Terrakion", "Virizion", "Tornadus", "Thundurus", "Landorus", "Xerneas", "Yveltal", "Celebi", "Zygarde"]
+cheap_legies=["Articuno", "Mew*", "Jirachi", "Moltres", 
+              "Raikou", "Entei", "Suicune", "Ho-oh", 
+              "Regirock", "Regice", "Registeel", 
+              "Latias", "Latios", "Deoxys", "Uxie", 
+              "Mesprit", "Azelf", "Heatran", "Regigigas", 
+              "Cresselia", "Cobalion", "Terrakion", "Virizion", 
+              "Tornadus", "Thundurus", "Landorus", "Xerneas", 
+              "Yveltal", "Celebi", "Zygarde"]
 
 cheap_legie_ball="prb" #Which ball to use on cheap legies
 
